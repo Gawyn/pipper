071644f3f6f3f273a7c6a7c0615dfd5d911525ae
Cristian Planas <cplanas@zendesk.com>
2015-03-24 17:40:26 +0100

    MongoDB setting

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import pymongo
 import twitter
 import yaml
+
+client = MongoClient()
+db = client.pipper
+tweets = db.tweets
 
 credentials = yaml.load(open("credentials.yml", "r"))
 api = twitter.Api(consumer_key=credentials['consumer_key'],
         consumer_secret=credentials['consumer_secret'],
         access_token_key=credentials['access_token_key'],
         access_token_secret=credentials['access_token_secret'])
+
+tweets.insert(tweet)
