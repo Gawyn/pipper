2f53b1f15b6bb16b498383a59d659915a40ab7a5
Gawyn <me@cristianplanas.com>
2015-03-24 19:31:49 +0100

    Query

--- setup.py
+++ setup.py
@@ -6,3 +6,6 @@ api = twitter.Api(consumer_key=credentials['consumer_key'],
         consumer_secret=credentials['consumer_secret'],
         access_token_key=credentials['access_token_key'],
         access_token_secret=credentials['access_token_secret'])
+
+final_tweet = 579762557763891200
+r = api.GetSearch("#ElClasico -filter:retweets", count=100, result_type="recent", include_entities=True, max_id=final_tweet)
